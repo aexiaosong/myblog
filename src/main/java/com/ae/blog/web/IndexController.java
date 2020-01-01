095d149b8e65506b568f92dc80a6696eeaa05d25
aexiaosong <2567053611@qq.com>
2020-01-01 15:42:53 +0800

    功能实现-全局搜索

--- src/main/java/com/ae/blog/web/IndexController.java
+++ src/main/java/com/ae/blog/web/IndexController.java
@@ -13,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -28,19 +30,38 @@ public class IndexController {
     @Autowired
     private TagService tagService;
 
+    /**
+     * 博客首页
+     * @param pageable pageable
+     * @param model model
+     * @return 访客访问的页面
+     */
     @GetMapping("/")
     public String index(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
                         Model model) {
         model.addAttribute("page",blogService.listBlog(pageable));
-        List<Type> types = typeService.listTypeTop(6);
-        model.addAttribute("types", types);
-        List<Tag> tags = tagService.listTagTop(10);
-        model.addAttribute("tags", tags);
+        model.addAttribute("types", typeService.listTypeTop(6));
+        model.addAttribute("tags", tagService.listTagTop(10));
         model.addAttribute("recommendBlogs", blogService.listRecommendBlogTop(8));
         return "index";
     }
 
-    @GetMapping("/blog")
+    /**
+     * 搜索
+     * @param pageable pageable
+     * @param query 搜索关键字
+     * @param model model
+     * @return 返回搜索结果的页面
+     */
+    @PostMapping("/search")
+    public String search(@PageableDefault(size = 8, sort = {"updateTime"}, direction = Sort.Direction.DESC) Pageable pageable,
+                         @RequestParam String query, Model model) {
+        model.addAttribute("page", blogService.listBlog("%"+query+"%", pageable));
+        model.addAttribute("query", query);
+        return "search";
+    }
+
+    @GetMapping("/blog/{id}")
     public String blog() {
         return "blog";
     }
